aef7f657f807437213f2bd0db1bd8c21d506c08d
maria_a_macedo <maria_a_macedo@estudante.sesisenai.org.br>
2023-03-22 12:36:35 -0300

    qqq

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView RT;
+    EditText N1, N2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        RT = findViewById(R.id.resultado);
+        N1 = findViewById(R.id.numero1);
+        N2 = findViewById(R.id.numero2);
     }
+    public void soma(View v){
+        int n1 = Integer.parseInt(N1.getText().toString());
+        int n2 = Integer.parseInt(N2.getText().toString());
+        int res = Integer.parseInt(RT.getText().toString());
+        int res = n1 + n2;
+        }
+        public void subtracao(View v){
+            int n1 = Integer.parseInt(N1.getText().toString());
+            int n2 = Integer.parseInt(N2.getText().toString());
+            int res = Integer.parseInt(RT.getText().toString());
+            int res = n1 - n2;
+        }
+        public void multiplicacao(View v){
+            int n1 = Integer.parseInt(numero1.getText().toString());
+            int n2 = Integer.parseInt(numero2.getText().toString());
+            int res = Integer.parseInt(resultado.getText().toString());
+            int res = n1 * n2;
+        }
+        public void divisao(View v){
+            int n1 = Integer.parseInt(numero1.getText().toString());
+            int n2 = Integer.parseInt(numero2.getText().toString());
+            int res = Integer.parseInt(resultado.getText().toString());
+            int res = n1 / n2;
+        }
 }
